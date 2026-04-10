1f7df2f61568ef63f7ee5fef53468c6e0aad9f95
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-10 21:25:53 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -156,6 +156,12 @@
     };
 
 
+fileSystems."/mnt/shared" = {
+  device = "share-home"; # This must match the 'Mount Tag' in VM settings
+  fsType = "virtiofs";
+  options = [ "nofail" ];
+};
+
 
   #grant admin sudo rights to vc drives
   services.udev.extraRules = ''
